cd0a79710bb6377573c79f5887b6321e5785b041
ganznz <loganbellinghamlb@gmail.com>
2024-04-04 16:25:06 +1300

    Define state with placeholder data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
 import "./App.css";
+import "./index.css";
+import {
+    PersonalDetails,
+    PrimaryEducation,
+    SecondaryEducation,
+    WorkExperience,
+} from "./models/state-models";
+import {
+    PersonalDetailsPlaceholderData,
+    PrimaryEducationPlaceholderData,
+    SecondaryEducationPlaceholderData,
+    WorkExperiencePlaceholderData,
+} from "./placeholder-data/state-placeholder-data";
 
 function App() {
-    return <div></div>;
+    const [personalDetails, setPersonalDetails] = useState<PersonalDetails>(
+        PersonalDetailsPlaceholderData
+    );
+    const [primaryEducation, setPrimaryEducation] = useState<
+        PrimaryEducation[]
+    >([PrimaryEducationPlaceholderData]);
+
+    const [secondaryEducation, setSecondaryEducation] = useState<
+        SecondaryEducation[]
+    >([SecondaryEducationPlaceholderData]);
+
+    const [workExperience, setWorkExperience] = useState<WorkExperience[]>([
+        WorkExperiencePlaceholderData,
+    ]);
+
+    return (
+        <>
+            <div className="bg-blue-500 text-white p-4">Hello</div>
+            <div className="bg-blue-500 text-black p-4">Hello</div>
+        </>
+    );
 }
 
 export default App;
