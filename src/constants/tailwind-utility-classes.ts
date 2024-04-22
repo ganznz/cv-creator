672ee02343efac5faced53dac1356f24f5bb0403
ganznz <loganbellinghamlb@gmail.com>
2024-04-22 22:58:10 +1200

    Added margin constants

--- src/constants/tailwind-utility-classes.ts
+++ src/constants/tailwind-utility-classes.ts
@@ -3,3 +3,17 @@ export const padding1 = "p-1";
 export const padding2 = "p-2";
 export const padding3 = "p-3";
 export const padding4 = "p-4";
+
+// margin
+export const margin1 = "m-1";
+export const margin2 = "m-2";
+export const margin3 = "m-3";
+export const margin4 = "m-4";
+export const margin1x = "mx-1";
+export const margin2x = "mx-2";
+export const margin3x = "mx-3";
+export const margin4x = "mx-4";
+export const margin1y = "my-1";
+export const margin2y = "my-2";
+export const margin3y = "my-3";
+export const margin4y = "my-4";
