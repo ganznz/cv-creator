9f3cf8b5d5e1e419ffcbaec053005e4da2b28ad4
ganznz <loganbellinghamlb@gmail.com>
2024-05-13 21:08:07 +1200

    Created PersonalDetailsContainer component. Added Personal Details form.

--- src/components/composite/FormEditSection/FormInformationContainer/PersonalDetailsContainer/PersonalDetailsContainer.tsx
+++ src/components/composite/FormEditSection/FormInformationContainer/PersonalDetailsContainer/PersonalDetailsContainer.tsx
@@ -1,5 +1,38 @@
-import Panel from "../../../../generic/Panel";
+import Form from "../../../../generic/Form/Form";
+import Input from "../../../../generic/Inputs/Input";
 
 export const PersonalDetailsContainer = () => {
-    return <Panel></Panel>;
+    return (
+        <Form className="flex flex-col gap-3">
+            <h1 className="text-2xl font-bold italic">Personal Details</h1>
+            <Input
+                type="text"
+                id="FullName"
+                label="Full Name"
+                description="recommended"
+                placeholder="Guardian of the Forest"
+            />
+            <Input
+                type="email"
+                id="Email"
+                label="Email"
+                description="recommended"
+                placeholder="lorax@whoville.com"
+            />
+            <Input
+                type="tel"
+                id="PhoneNumber"
+                label="Phone Number"
+                description="recommended"
+                placeholder="+64 123 4567"
+            />
+            <Input
+                type="text"
+                id="Address"
+                label="Address"
+                description="recommended"
+                placeholder="2 Lorax Lane, Whoville"
+            />
+        </Form>
+    );
 };
