42a29c8698d4c3a5b339be3e0b6bcc2904b43482
ganznz <loganbellinghamlb@gmail.com>
2024-05-13 21:12:02 +1200

    Added PersonalDetailsContainer form

--- src/components/composite/FormEditSection/FormInformationContainer/FormInformationContainer.tsx
+++ src/components/composite/FormEditSection/FormInformationContainer/FormInformationContainer.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import Navbar from "../../../generic/Navbar/Navbar";
 import Button from "../../../generic/Buttons/Button";
+import { PersonalDetailsContainer } from "./PersonalDetailsContainer/PersonalDetailsContainer";
 // interface FormInformationContainerProps {}
 
 export const FormInformationContainer = () => {
+    // "education", "experience"
+    const [activeDropdown, setActiveDropdown] = useState(null);
+
     return (
-        <div className="flex flex-col">
-            {/* dropdown component for education here (takes children <list item> components for each different school) */}
-            {/* clicking these <list item>'s will display <form /> component to edit information for that school */}
+        <div className="flex flex-col w-full">
+            {/* dropdown component for EDUCATION and WORK EXP here (takes children <list item> components for each place of education/job) */}
+            {/* clicking these <list item>'s will display <form /> component to edit information for that place */}
+            <PersonalDetailsContainer></PersonalDetailsContainer>
         </div>
     );
 };
