dc05a910b3a2fa30e886894227c535c115e7a238
ganznz <loganbellinghamlb@gmail.com>
2024-05-16 22:52:34 +1200

    Now dynamically displays Dropdowns

--- src/components/composite/FormEditSection/FormInformationContainer/FormInformationContainer.tsx
+++ src/components/composite/FormEditSection/FormInformationContainer/FormInformationContainer.tsx
@@ -2,48 +2,114 @@ import { useState } from "react";
 import { PersonalDetailsContainer } from "./PersonalDetailsContainer/PersonalDetailsContainer";
 import FormDropdown from "../../../generic/Dropdown/FormDropdown";
 import DropdownItem from "../../../generic/Dropdown/DropdownItem";
+import {
+    PersonalDetails,
+    PrimaryEducation,
+    SecondaryEducation,
+    WorkExperience,
+} from "../../../../models/state-models";
+import { produce } from "immer";
 
-// interface FormInformationContainerProps {}
+interface FormInformationContainerProps {
+    personalDetails: PersonalDetails | null;
+    primaryEducation: { [k: string]: PrimaryEducation } | null;
+    secondaryEducation: { [k: string]: SecondaryEducation } | null;
+    workExperience: { [k: string]: WorkExperience } | null;
+    setPersonalDetails: React.Dispatch<
+        React.SetStateAction<PersonalDetails | null>
+    >;
+    setPrimaryEducation: React.Dispatch<
+        React.SetStateAction<{ [k: string]: PrimaryEducation } | null>
+    >;
+    setSecondaryEducation: React.Dispatch<
+        React.SetStateAction<{ [k: string]: SecondaryEducation } | null>
+    >;
+    setWorkExperience: React.Dispatch<
+        React.SetStateAction<{ [k: string]: WorkExperience } | null>
+    >;
+}
 
-export const FormInformationContainer = () => {
-    // "education", "experience"
-    const [activeDropdown, setActiveDropdown] = useState<null | string>(null);
+export const FormInformationContainer = ({
+    personalDetails,
+    primaryEducation,
+    secondaryEducation,
+    workExperience,
+    setPersonalDetails,
+    setPrimaryEducation,
+    setSecondaryEducation,
+    setWorkExperience,
+}: FormInformationContainerProps) => {
+    const [activeDropdown, setActiveDropdown] = useState<
+        null | "primaryEducation" | "secondaryEducation" | "workExperience"
+    >(null);
 
     return (
         <div className="flex flex-col w-full">
             {/* personal details form */}
-            <PersonalDetailsContainer></PersonalDetailsContainer>
+            <PersonalDetailsContainer
+                personalDetails={personalDetails}
+                setPersonalDetails={setPersonalDetails}
+            ></PersonalDetailsContainer>
 
-            {/* dropdown component for EDUCATION and WORK EXP here (takes children <list item> components for each place of education/job) */}
-            {/* clicking these <list item>'s will display <form /> component to edit information for that place */}
+            {primaryEducation && (
+                <FormDropdown
+                    name="Primary Education"
+                    onClick={() =>
+                        setActiveDropdown(
+                            activeDropdown === "primaryEducation"
+                                ? null
+                                : "primaryEducation"
+                        )
+                    }
+                    expanded={activeDropdown === "primaryEducation"}
+                >
+                    {Object.keys(primaryEducation).map(
+                        (primaryInstitutionName) => (
+                            <DropdownItem name={primaryInstitutionName} />
+                        )
+                    )}
+                </FormDropdown>
+            )}
 
-            <FormDropdown
-                name="Education"
-                onClick={() =>
-                    setActiveDropdown(
-                        activeDropdown === "education" ? null : "education"
-                    )
-                }
-                expanded={activeDropdown === "education"}
-            >
-                <DropdownItem name="Edu1" />
-                <DropdownItem name="Edu2" />
-                <DropdownItem name="Edu3" />
-            </FormDropdown>
+            {secondaryEducation && (
+                <FormDropdown
+                    name="Secondary Education"
+                    onClick={() =>
+                        setActiveDropdown(
+                            activeDropdown === "secondaryEducation"
+                                ? null
+                                : "secondaryEducation"
+                        )
+                    }
+                    expanded={activeDropdown === "secondaryEducation"}
+                >
+                    {Object.keys(secondaryEducation).map(
+                        (secondaryInstitutionName) => (
+                            <DropdownItem name={secondaryInstitutionName} />
+                        )
+                    )}
+                </FormDropdown>
+            )}
 
-            <FormDropdown
-                name="Experience"
-                onClick={() =>
-                    setActiveDropdown(
-                        activeDropdown === "experience" ? null : "experience"
-                    )
-                }
-                expanded={activeDropdown === "experience"}
-            >
-                <DropdownItem name="Exp1" />
-                <DropdownItem name="Exp2" />
-                <DropdownItem name="Exp3" />
-            </FormDropdown>
+            {workExperience && (
+                <FormDropdown
+                    name="Work Experience"
+                    onClick={() =>
+                        setActiveDropdown(
+                            activeDropdown === "workExperience"
+                                ? null
+                                : "workExperience"
+                        )
+                    }
+                    expanded={activeDropdown === "workExperience"}
+                >
+                    {Object.keys(workExperience).map(
+                        (secondaryInstitutionName) => (
+                            <DropdownItem name={secondaryInstitutionName} />
+                        )
+                    )}
+                </FormDropdown>
+            )}
         </div>
     );
 };
