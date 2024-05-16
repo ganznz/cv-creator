df6f5b9ea5e9ea24aa29bca47c57191512787d1c
ganznz <loganbellinghamlb@gmail.com>
2024-05-16 22:53:27 +1200

    Passed form data as props. Added LoadExample & ClearResume btn functionality

--- src/components/composite/FormEditSection/FormEditSection.tsx
+++ src/components/composite/FormEditSection/FormEditSection.tsx
@@ -3,8 +3,73 @@ import Navbar from "../../generic/Navbar/Navbar";
 import Button from "../../generic/Buttons/Button";
 import { FormInformationContainer } from "./FormInformationContainer/FormInformationContainer";
 import { gap6 } from "../../../constants/tailwind-utility-classes";
+import {
+    PersonalDetails,
+    PrimaryEducation,
+    SecondaryEducation,
+    WorkExperience,
+} from "../../../models/state-models";
+import {
+    PersonalDetailsPlaceholderData,
+    PrimaryEducationPlaceholderData,
+    SecondaryEducationPlaceholderData,
+    WorkExperiencePlaceholderData,
+} from "../../../placeholder-data/state-placeholder-data";
 
-export const FormEditSection = () => {
+interface FormEditSectionProps {
+    personalDetails: PersonalDetails | null;
+    primaryEducation: { [k: string]: PrimaryEducation } | null;
+    secondaryEducation: { [k: string]: SecondaryEducation } | null;
+    workExperience: { [k: string]: WorkExperience } | null;
+    setPersonalDetails: React.Dispatch<
+        React.SetStateAction<PersonalDetails | null>
+    >;
+    setPrimaryEducation: React.Dispatch<
+        React.SetStateAction<{ [k: string]: PrimaryEducation } | null>
+    >;
+    setSecondaryEducation: React.Dispatch<
+        React.SetStateAction<{ [k: string]: SecondaryEducation } | null>
+    >;
+    setWorkExperience: React.Dispatch<
+        React.SetStateAction<{ [k: string]: WorkExperience } | null>
+    >;
+}
+
+export const FormEditSection = ({
+    personalDetails,
+    primaryEducation,
+    secondaryEducation,
+    workExperience,
+    setPersonalDetails,
+    setPrimaryEducation,
+    setSecondaryEducation,
+    setWorkExperience,
+}: FormEditSectionProps) => {
+    // remove form state of all their data
+    const wipeAllForms = () => {
+        setPersonalDetails(null);
+        setPrimaryEducation(null);
+        setSecondaryEducation(null);
+        setWorkExperience(null);
+    };
+
+    // populate form state with placeholder data
+    const populateAllForms = () => {
+        setPersonalDetails(PersonalDetailsPlaceholderData);
+        setPrimaryEducation({
+            [PrimaryEducationPlaceholderData.InstituteName]:
+                PrimaryEducationPlaceholderData,
+        });
+        setSecondaryEducation({
+            [SecondaryEducationPlaceholderData.InstituteName]:
+                SecondaryEducationPlaceholderData,
+        });
+        setWorkExperience({
+            [WorkExperiencePlaceholderData.JobName]:
+                WorkExperiencePlaceholderData,
+        });
+    };
+
     // "content", "customize"
     const [activePanel, setActiveWindow] = useState("content");
 
@@ -35,13 +100,18 @@ export const FormEditSection = () => {
 
             <div className={`w-[20rem] flex flex-col items-center ${gap6}`}>
                 <Navbar direction="horizontal" classes={["w-full"]}>
-                    <Button variant="success" visibleHover={true}>
+                    <Button
+                        variant="success"
+                        visibleHover={true}
+                        onClick={populateAllForms}
+                    >
                         Load Example
                     </Button>
                     <Button
                         variant="warn"
                         visibleBackground={true}
                         visibleHover={true}
+                        onClick={wipeAllForms}
                     >
                         <span className="flex gap-2 justify-center content-center">
                             <i className="fa-solid fa-user"></i>
@@ -52,7 +122,16 @@ export const FormEditSection = () => {
 
                 {/* add content section here (edit personal details, education, experience, etc) */}
                 {activePanel == "content" && (
-                    <FormInformationContainer></FormInformationContainer>
+                    <FormInformationContainer
+                        personalDetails={personalDetails}
+                        primaryEducation={primaryEducation}
+                        secondaryEducation={secondaryEducation}
+                        workExperience={workExperience}
+                        setPersonalDetails={setPersonalDetails}
+                        setPrimaryEducation={setPrimaryEducation}
+                        setSecondaryEducation={setSecondaryEducation}
+                        setWorkExperience={setWorkExperience}
+                    ></FormInformationContainer>
                 )}
             </div>
         </div>
