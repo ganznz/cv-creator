0728b95c1cc84943658e4f23d5ebd22d42d38784
ganznz <loganbellinghamlb@gmail.com>
2024-05-20 21:27:11 +1200

    children prop gets rendered dynamically, passing onClick prop to each child that determines visibleForm state

--- src/components/generic/Dropdown/FormDropdown.tsx
+++ src/components/generic/Dropdown/FormDropdown.tsx
@@ -1,8 +1,10 @@
+import React, { useState } from "react";
 import ExpandableDropdown, {
     ExpandableDropdownProps,
 } from "./ExpandableDropdown";
-
+import Form from "../Form/Form";
 import Button from "../Buttons/Button";
+import { DropdownItemProps } from "./DropdownItem";
 
 interface FormDropdownProps extends ExpandableDropdownProps {}
 
@@ -10,9 +12,24 @@ export default function FormDropdown({
     children,
     ...props
 }: FormDropdownProps) {
+    const [visibleForm, setVisibleForm] = useState("");
+
+    // if visibleForm exists and expanded prop is true, show the form
+    visibleForm && { ...props }.expanded && <Form></Form>;
+
+    // else, render dropdown
     return (
         <ExpandableDropdown {...props}>
-            {children}
+            {React.Children.map(children, (child) => {
+                if (React.isValidElement<DropdownItemProps>(child)) {
+                    return React.cloneElement(child, {
+                        onClick: () => {
+                            if (visibleForm == child.props.name) return;
+                            setVisibleForm(child.props.name);
+                        },
+                    });
+                }
+            })}
             <div className="flex justify-center items-center">
                 <Button variant="success" visibleHover={true}>
                     <i className="fa-solid fa-plus"></i>
