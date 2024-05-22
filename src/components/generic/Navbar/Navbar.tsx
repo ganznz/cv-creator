8e1dfd63758a3c805711eea1a05e3caf4398a8d5
ganznz <loganbellinghamlb@gmail.com>
2024-05-23 00:09:42 +1200

    Navbar component now takes className prop using HTMLProps<HTMLElement> instead of custom defined class prop

--- src/components/generic/Navbar/Navbar.tsx
+++ src/components/generic/Navbar/Navbar.tsx
@@ -1,30 +1,22 @@
 import React from "react";
 import Panel from "../Panel";
-import { TailwindUtilityClasses } from "../../types/GenericTypes";
 
-interface NavbarProps {
+interface NavbarProps extends React.HTMLAttributes<HTMLDivElement> {
     direction: "horizontal" | "vertical";
-
     children: React.ReactNode;
-    classes?: TailwindUtilityClasses;
 }
 
-export default function Navbar({ direction, children, classes }: NavbarProps) {
-    const defaultClasses = [
-        "flex",
-        "gap-3",
-        (direction == "vertical" && "flex-col") || "flex-row",
-    ];
+export default function Navbar({ direction, children, ...props }: NavbarProps) {
+    let classes = `flex gap-3 ${
+        (direction == "vertical" && "flex-col") || "flex-row"
+    }`;
+    classes = { ...props }.className
+        ? `${classes} ${{ ...props }.className}`
+        : classes;
+
     return (
         <>
-            <Panel
-                classes={
-                    (classes && defaultClasses.concat(classes)) ||
-                    defaultClasses
-                }
-            >
-                {children}
-            </Panel>
+            <Panel className={classes}>{children}</Panel>
         </>
     );
 }
