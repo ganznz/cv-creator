c7aadeabfabab51c6a8f6816028a99603c8d3517
ganznz <loganbellinghamlb@gmail.com>
2024-05-23 14:56:33 +1200

    Added utility classes

--- src/constants/tailwind-utility-classes.ts
+++ src/constants/tailwind-utility-classes.ts
@@ -1,22 +1,25 @@
 // padding
 export const padding1 = "p-1";
 export const padding2 = "p-2";
-export const padding3 = "p-3";
 export const padding4 = "p-4";
+export const xPadding1 = "px-1";
+export const xPadding2 = "px-2";
+export const yPadding1 = "py-1";
+export const yPadding2 = "py-2";
 
 // margin
 export const margin1 = "m-1";
 export const margin2 = "m-2";
 export const margin3 = "m-3";
 export const margin4 = "m-4";
-export const margin1x = "mx-1";
-export const margin2x = "mx-2";
-export const margin3x = "mx-3";
-export const margin4x = "mx-4";
-export const margin1y = "my-1";
-export const margin2y = "my-2";
-export const margin3y = "my-3";
-export const margin4y = "my-4";
+export const xMargin1 = "mx-1";
+export const xMargin2 = "mx-2";
+export const xMargin3 = "mx-3";
+export const xMargin4 = "mx-4";
+export const yMargin1 = "my-1";
+export const yMargin2 = "my-2";
+export const yMargin3 = "my-3";
+export const yMargin4 = "my-4";
 
 // gap
 export const gap1 = "gap-1";
