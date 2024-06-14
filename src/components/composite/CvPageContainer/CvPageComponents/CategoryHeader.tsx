7f15cb72ef81f7574a985e237f274e1057a71f42
ganznz <loganbellinghamlb@gmail.com>
2024-06-14 15:58:04 +1200

    Component now take primaryColour & secondaryColour props for custom colour styling

--- src/components/composite/CvPageContainer/CvPageComponents/CategoryHeader.tsx
+++ src/components/composite/CvPageContainer/CvPageComponents/CategoryHeader.tsx
@@ -3,15 +3,24 @@ import { twMerge } from "tailwind-merge";
 
 interface CategoryHeaderProps extends HTMLProps<HTMLDivElement> {
     text: string;
+    primaryColour?: string;
+    secondaryColour?: string;
 }
 
-export const CategoryHeader = ({ text, className }: CategoryHeaderProps) => {
+export const CategoryHeader = ({
+    text,
+    className,
+    primaryColour = "#000",
+    secondaryColour = "#fff",
+}: CategoryHeaderProps) => {
     let classes =
         "flex justify-center content-center bg-[#f0f0f0] font-bold p-1";
     classes = className ? twMerge(classes, className) : classes;
     return (
-        <div className={classes}>
-            <h3 className="text-xl">{text}</h3>
+        <div style={{ backgroundColor: secondaryColour }} className={classes}>
+            <h3 style={{ color: primaryColour }} className="text-xl">
+                {text}
+            </h3>
         </div>
     );
 };
--- src/components/composite/CvPageContainer/CvPageComponents/CvPageHeader.tsx
+++ src/components/composite/CvPageContainer/CvPageComponents/CvPageHeader.tsx
@@ -1,30 +1,52 @@
+import { HTMLProps } from "react";
 import { PersonalDetails } from "../../../../models/state-models";
 
-interface CvPageHeaderProps {
+interface CvPageHeaderProps extends HTMLProps<"div"> {
     personalDetails: PersonalDetails;
+    primaryColour?: string;
+    secondaryColour?: string;
 }
 
-export const CvPageHeader = ({ personalDetails }: CvPageHeaderProps) => {
+export const CvPageHeader = ({
+    personalDetails,
+    primaryColour = "#000",
+    secondaryColour = "#fff",
+}: CvPageHeaderProps) => {
     return (
-        <header className="w-full bg-red-500 flex flex-col content-between text-center p-8">
-            <h1 className="mb-6 italic font-bold text-4xl">
+        <header
+            style={{ backgroundColor: primaryColour }}
+            className={`w-full flex flex-col content-between text-center p-8`}
+        >
+            <h1
+                style={{ color: `${secondaryColour}` }}
+                className="mb-6 italic font-bold text-4xl"
+            >
                 {personalDetails["Full Name"]}
             </h1>
             <div className="flex justify-evenly gap-3">
                 {personalDetails["Phone Number"] !== "" && (
-                    <span className="flex gap-2 items-center">
+                    <span
+                        style={{ color: secondaryColour }}
+                        className="flex gap-2 items-center"
+                    >
                         <i className="fa-solid fa-phone"></i>
                         <h4>{personalDetails["Phone Number"]}</h4>
                     </span>
                 )}
                 {personalDetails.Email !== "" && (
-                    <span className="flex gap-2 items-center">
+                    <span
+                        style={{ color: secondaryColour }}
+                        className="flex gap-2 items-center"
+                    >
                         <i className="fa-solid fa-envelope"></i>
                         <h4>{personalDetails.Email}</h4>
                     </span>
                 )}
                 {personalDetails.Address !== "" && (
-                    <span className="flex gap-2 items-center">
+                    <span
+                        style={{ color: secondaryColour }}
+                        className="flex gap-2 items-center"
+                    >
                         <i className="fa-solid fa-house"></i>
                         <h4>{personalDetails.Address}</h4>
                     </span>
