2e2cfa3bbaa620b23b17f993c5c40280a72ed91f
ganznz <loganbellinghamlb@gmail.com>
2024-06-15 00:45:23 +1200

    Added print resume page functionality

--- src/components/composite/CvPageContainer/CvPageContainer.tsx
+++ src/components/composite/CvPageContainer/CvPageContainer.tsx
@@ -7,6 +7,10 @@ import {
 import { CvPageHeader } from "./CvPageComponents/CvPageHeader";
 import { CategoryHeader } from "./CvPageComponents/CategoryHeader";
 import ExperienceInfo from "./CvPageComponents/ExperienceInfo";
+import Button from "../../generic/Buttons/Button";
+
+import { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
 
 interface CvPageContainerProps {
     personalDetails: PersonalDetails;
@@ -27,94 +31,113 @@ export const CvPageContainer = ({
     resumeDataVisibility,
     resumeColours,
 }: CvPageContainerProps) => {
+    const contentToPrint = useRef(null);
+    const handlePrint = useReactToPrint({
+        content: () => contentToPrint.current,
+        removeAfterPrint: true,
+    });
+
     return (
-        <div
-            style={{ width: "calc(28cm * 0.707)", height: "28cm" }}
-            className={`bg-white drop-shadow hover:drop-shadow-xl transition-all`}
-        >
-            {/* header */}
-            <CvPageHeader
-                personalDetails={personalDetails}
-                primaryColour={resumeColours[0]}
-                secondaryColour={resumeColours[1]}
-            />
+        <div className="flex flex-col gap-5">
+            <Button
+                visibleBackground={true}
+                visibleHover={true}
+                onClick={handlePrint}
+            >
+                <p className="font-bold italic">Print your CV!</p>
+            </Button>
+            <div
+                style={{ width: "calc(32cm * 0.707)", height: "32cm" }}
+                className="print-container bg-white drop-shadow hover:drop-shadow-xl transition-all"
+                ref={contentToPrint}
+            >
+                <div className="print-content">
+                    {/* header */}
+                    <CvPageHeader
+                        personalDetails={personalDetails}
+                        primaryColour={resumeColours[0]}
+                        secondaryColour={resumeColours[1]}
+                    />
 
-            {/* cv page content */}
-            <div className="flex flex-col gap-8 p-12">
-                {Object.keys(primaryEducation).length +
-                    Object.keys(secondaryEducation).length >=
-                    1 && (
-                    <span className="flex flex-col gap-2">
-                        <CategoryHeader
-                            text="Education"
-                            primaryColour={resumeColours[0]}
-                            secondaryColour={resumeColours[1]}
-                        ></CategoryHeader>
+                    {/* cv page content */}
+                    <div className="flex flex-col gap-8 p-12">
+                        {Object.keys(primaryEducation).length +
+                            Object.keys(secondaryEducation).length >=
+                            1 && (
+                            <span className="flex flex-col gap-2">
+                                <CategoryHeader
+                                    text="Education"
+                                    primaryColour={resumeColours[0]}
+                                    secondaryColour={resumeColours[1]}
+                                ></CategoryHeader>
 
-                        {/* primary education */}
-                        {Object.keys(primaryEducation).length >= 1 && (
-                            <h4 className="text-md font-bold italic">
-                                Primary Education
-                            </h4>
+                                {/* primary education */}
+                                {Object.keys(primaryEducation).length >= 1 && (
+                                    <h4 className="text-md font-bold italic">
+                                        Primary Education
+                                    </h4>
+                                )}
+                                <div className="flex flex-col gap-4">
+                                    {Object.entries(primaryEducation).map(
+                                        ([dataUUID, dataObj]) =>
+                                            resumeDataVisibility[dataUUID] && (
+                                                <ExperienceInfo
+                                                    key={dataUUID}
+                                                    infoType="primaryEducation"
+                                                    infoData={dataObj}
+                                                    metaInfoWidth={175}
+                                                />
+                                            )
+                                    )}
+                                </div>
+
+                                {/* tertiary education */}
+                                {Object.keys(secondaryEducation).length >=
+                                    1 && (
+                                    <h4 className="text-md font-bold italic">
+                                        Tertiary Education
+                                    </h4>
+                                )}
+                                <div className="flex flex-col gap-4">
+                                    {Object.entries(secondaryEducation).map(
+                                        ([dataUUID, dataObj]) =>
+                                            resumeDataVisibility[dataUUID] && (
+                                                <ExperienceInfo
+                                                    key={dataUUID}
+                                                    infoType="secondaryEducation"
+                                                    infoData={dataObj}
+                                                    metaInfoWidth={175}
+                                                />
+                                            )
+                                    )}
+                                </div>
+                            </span>
                         )}
-                        <div className="flex flex-col gap-4">
-                            {Object.entries(primaryEducation).map(
-                                ([dataUUID, dataObj]) =>
-                                    resumeDataVisibility[dataUUID] && (
-                                        <ExperienceInfo
-                                            key={dataUUID}
-                                            infoType="primaryEducation"
-                                            infoData={dataObj}
-                                            metaInfoWidth={175}
-                                        />
-                                    )
-                            )}
-                        </div>
 
-                        {/* tertiary education */}
-                        {Object.keys(secondaryEducation).length >= 1 && (
-                            <h4 className="text-md font-bold italic">
-                                Tertiary Education
-                            </h4>
+                        {Object.keys(workExperience).length >= 1 && (
+                            <>
+                                <CategoryHeader
+                                    text="Work Experience"
+                                    primaryColour={resumeColours[0]}
+                                    secondaryColour={resumeColours[1]}
+                                ></CategoryHeader>
+                                <div className="flex flex-col gap-4">
+                                    {Object.entries(workExperience).map(
+                                        ([dataUUID, dataObj]) =>
+                                            resumeDataVisibility[dataUUID] && (
+                                                <ExperienceInfo
+                                                    key={dataUUID}
+                                                    infoType="workExperience"
+                                                    infoData={dataObj}
+                                                    metaInfoWidth={175}
+                                                />
+                                            )
+                                    )}
+                                </div>
+                            </>
                         )}
-                        <div className="flex flex-col gap-4">
-                            {Object.entries(secondaryEducation).map(
-                                ([dataUUID, dataObj]) =>
-                                    resumeDataVisibility[dataUUID] && (
-                                        <ExperienceInfo
-                                            key={dataUUID}
-                                            infoType="secondaryEducation"
-                                            infoData={dataObj}
-                                            metaInfoWidth={175}
-                                        />
-                                    )
-                            )}
-                        </div>
-                    </span>
-                )}
-
-                {Object.keys(workExperience).length >= 1 && (
-                    <>
-                        <CategoryHeader
-                            text="Work Experience"
-                            primaryColour={resumeColours[0]}
-                            secondaryColour={resumeColours[1]}
-                        ></CategoryHeader>
-                        <div className="flex flex-col gap-4">
-                            {Object.entries(workExperience).map(
-                                ([dataUUID, dataObj]) =>
-                                    resumeDataVisibility[dataUUID] && (
-                                        <ExperienceInfo
-                                            key={dataUUID}
-                                            infoType="workExperience"
-                                            infoData={dataObj}
-                                            metaInfoWidth={175}
-                                        />
-                                    )
-                            )}
-                        </div>
-                    </>
-                )}
+                    </div>
+                </div>
             </div>
         </div>
     );
